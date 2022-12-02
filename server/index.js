6e07f02c46d456fac8c3a6911faeb2f252021f31
salvadorM271 <guardianes27@outlook.com>
2022-12-01 18:06:56 -0600

    first commit

--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.s9mxdyu.mongodb.net/memories-dev?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 80;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
